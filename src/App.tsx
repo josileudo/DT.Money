4103982f54bcf7e6fd6d6f0a7451caff6a68d175
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2021-05-27 11:26:15 -0300

    context api in react

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Header } from "./Components/Header";
 import Modal from "react-modal"
 import { GlobalStyle } from "./styles/global";
 import { NewTransactionModal } from "./Components/NewTransactionModal";
+import { TransactionsContext } from "./TransactionsContexts";
 
 Modal.setAppElement('#root') // Ele passa o modal por cima da página
 
@@ -19,13 +20,13 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsContext.Provider value = {[]} >
       <Header onOpenNewTransactionModal = {handleOpenNewTransactionModal}/>
       <Dashboard />
       <NewTransactionModal 
         isOpen = {isNewTransactionModalOpen} 
         onRequestClose = {handleCloseNewTransactionModal}/>
       <GlobalStyle />
-    </>
+    </TransactionsContext.Provider>
   );
 }
